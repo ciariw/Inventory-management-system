0790dbfdd293704ad80acdbbc11fbdd951825ff3
Emeka <eariwodo98@gmail.com>
2022-06-02 12:15:15 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# My goals:
+# - create a data structure for parts that includes reference images, material code and name
+# - initial items can be categorized using the
+# - items are referenced
+#
+# Later goals:
+# - include image recognition
+# - include GUI for displaying images and names. Train based on selection
+# - quick scan
+#
+
+import json
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
